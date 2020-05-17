1f38de541ca6de8a0f45ad6d9a0373e54781ad34
unknown <izaiaslc@yahoo.com.br>
2020-05-17 12:03:05 -0300

    Versão inicial

---
+++ Desafio/Form1.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace Desafio
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtToken = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtDecifrado = new System.Windows.Forms.TextBox();
+            this.txtCifrado = new System.Windows.Forms.TextBox();
+            this.txtNumeroCasas = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtResumo = new System.Windows.Forms.TextBox();
+            this.btnDecifrar = new System.Windows.Forms.Button();
+            this.btnCifrar = new System.Windows.Forms.Button();
+            this.btnHash = new System.Windows.Forms.Button();
+            this.btnGerarJson = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtToken
+            // 
+            this.txtToken.Location = new System.Drawing.Point(84, 17);
+            this.txtToken.Name = "txtToken";
+            this.txtToken.Size = new System.Drawing.Size(339, 20);
+            this.txtToken.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(37, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Token:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(429, 14);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Obter Json";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtDecifrado
+            // 
+            this.txtDecifrado.Location = new System.Drawing.Point(132, 229);
+            this.txtDecifrado.Multiline = true;
+            this.txtDecifrado.Name = "txtDecifrado";
+            this.txtDecifrado.Size = new System.Drawing.Size(618, 52);
+            this.txtDecifrado.TabIndex = 3;
+            // 
+            // txtCifrado
+            // 
+            this.txtCifrado.Location = new System.Drawing.Point(132, 164);
+            this.txtCifrado.Multiline = true;
+            this.txtCifrado.Name = "txtCifrado";
+            this.txtCifrado.Size = new System.Drawing.Size(618, 52);
+            this.txtCifrado.TabIndex = 4;
+            // 
+            // txtNumeroCasas
+            // 
+            this.txtNumeroCasas.Location = new System.Drawing.Point(132, 116);
+            this.txtNumeroCasas.Name = "txtNumeroCasas";
+            this.txtNumeroCasas.ReadOnly = true;
+            this.txtNumeroCasas.Size = new System.Drawing.Size(100, 20);
+            this.txtNumeroCasas.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(33, 119);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Número de casas:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(81, 167);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Cifrado:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(68, 232);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Decifrado:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(68, 302);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(49, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Resumo:";
+            // 
+            // txtResumo
+            // 
+            this.txtResumo.Location = new System.Drawing.Point(132, 299);
+            this.txtResumo.Name = "txtResumo";
+            this.txtResumo.Size = new System.Drawing.Size(331, 20);
+            this.txtResumo.TabIndex = 10;
+            // 
+            // btnDecifrar
+            // 
+            this.btnDecifrar.Location = new System.Drawing.Point(132, 366);
+            this.btnDecifrar.Name = "btnDecifrar";
+            this.btnDecifrar.Size = new System.Drawing.Size(75, 23);
+            this.btnDecifrar.TabIndex = 11;
+            this.btnDecifrar.Text = "Decifrar";
+            this.btnDecifrar.UseMnemonic = false;
+            this.btnDecifrar.UseVisualStyleBackColor = true;
+            this.btnDecifrar.Click += new System.EventHandler(this.btnDecifrar_Click);
+            // 
+            // btnCifrar
+            // 
+            this.btnCifrar.Location = new System.Drawing.Point(388, 366);
+            this.btnCifrar.Name = "btnCifrar";
+            this.btnCifrar.Size = new System.Drawing.Size(75, 23);
+            this.btnCifrar.TabIndex = 12;
+            this.btnCifrar.Text = "Cifrar";
+            this.btnCifrar.UseVisualStyleBackColor = true;
+            this.btnCifrar.Visible = false;
+            this.btnCifrar.Click += new System.EventHandler(this.btnCifrar_Click);
+            // 
+            // btnHash
+            // 
+            this.btnHash.Location = new System.Drawing.Point(213, 366);
+            this.btnHash.Name = "btnHash";
+            this.btnHash.Size = new System.Drawing.Size(75, 23);
+            this.btnHash.TabIndex = 13;
+            this.btnHash.Text = "Hash";
+            this.btnHash.UseVisualStyleBackColor = true;
+            this.btnHash.Click += new System.EventHandler(this.btnHash_Click);
+            // 
+            // btnGerarJson
+            // 
+            this.btnGerarJson.Location = new System.Drawing.Point(294, 366);
+            this.btnGerarJson.Name = "btnGerarJson";
+            this.btnGerarJson.Size = new System.Drawing.Size(75, 23);
+            this.btnGerarJson.TabIndex = 15;
+            this.btnGerarJson.Text = "Gerar Json";
+            this.btnGerarJson.UseVisualStyleBackColor = true;
+            this.btnGerarJson.Click += new System.EventHandler(this.btnGerarJson_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 540);
+            this.Controls.Add(this.btnGerarJson);
+            this.Controls.Add(this.btnHash);
+            this.Controls.Add(this.btnCifrar);
+            this.Controls.Add(this.btnDecifrar);
+            this.Controls.Add(this.txtResumo);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtNumeroCasas);
+            this.Controls.Add(this.txtCifrado);
+            this.Controls.Add(this.txtDecifrado);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtToken);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtToken;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtDecifrado;
+        private System.Windows.Forms.TextBox txtCifrado;
+        private System.Windows.Forms.TextBox txtNumeroCasas;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtResumo;
+        private System.Windows.Forms.Button btnDecifrar;
+        private System.Windows.Forms.Button btnCifrar;
+        private System.Windows.Forms.Button btnHash;
+        private System.Windows.Forms.Button btnGerarJson;
+    }
+}
+
---
+++ Desafio/Form1.cs
@@ -0,0 +1,276 @@
+﻿using Desafio.Classes;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+using System.Windows.Forms;
+using System.Collections;
+ 
+
+namespace Desafio
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (string.IsNullOrWhiteSpace(txtToken.Text))
+                {
+                    MessageBox.Show("Informe o token");
+                }
+                else
+                {
+                    var json = ObterJson(txtToken.Text);
+
+                    if (json != null)
+                    {
+
+                        txtNumeroCasas.Text = json.numero_casas.ToString();
+                        txtCifrado.Text = json.cifrado;
+                        txtDecifrado.Text = json.decifrado;
+                        txtResumo.Text = json.resumo_criptografico;
+
+                        txtToken.ReadOnly = true;
+                    }
+                }
+           
+             }catch(Exception err)
+            {
+                MessageBox.Show("Não foi possível obter arquivo com o token informado");
+            }
+
+}
+
+        private Json ObterJson(string token)
+        {
+
+            string url = string.Format("https://api.codenation.dev/v1/challenge/dev-ps/generate-data?token={0}", token);
+            WebRequest request = WebRequest.Create(url);
+
+            request.Credentials = CredentialCache.DefaultCredentials;
+
+            Desafio.Classes.Json json = new Classes.Json();
+
+            WebResponse response = request.GetResponse();
+
+            using (Stream dataStream = response.GetResponseStream())
+            {
+
+                StreamReader reader = new StreamReader(dataStream);
+
+                var retorno = reader.ReadToEnd();
+
+                JavaScriptSerializer js = new JavaScriptSerializer();
+
+                json = js.Deserialize<Json>(retorno);
+            }
+
+
+            return json;
+
+        }
+
+   
+        private void btnDecifrar_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtCifrado.Text))
+            {
+                MessageBox.Show("Informe o texto cifrado");
+            }
+            else
+            {
+                txtDecifrado.Text = Descifrar(txtCifrado.Text, int.Parse(txtNumeroCasas.Text));
+            }
+        }
+
+        private string Descifrar(string texto, int salt)
+        {
+
+            int letra;
+            string decript = string.Empty;
+            for (int i = 0; i < texto.Length; i++)
+            {
+                var number = texto[i];
+                letra = Convert.ToInt32(texto[i]) - salt;
+
+                //se passar do 'z' volta ao inicio do alfabeto
+
+                if (letra > 122) { letra = letra - 26; }
+
+                if (letra < 97) { letra = letra + 26; }
+
+                decript = Char.IsNumber(number) || Char.IsWhiteSpace(number) || number == '.' ? decript += number : decript += Convert.ToChar(letra);
+
+            }
+
+            return decript;
+        }
+
+        private string Cifrar(string texto, int salt)
+        {
+
+            int letra;
+            string decript = string.Empty;
+            for (int i = 0; i < texto.Length; i++)
+            {
+                var number = texto[i];
+                letra = Convert.ToInt32(texto[i]) + salt;
+
+                //se passar do 'z' volta ao inicio do alfabeto
+
+                if (letra > 122) { letra = letra - 26; }
+
+                if (letra < 97) { letra = letra + 26; }
+
+                decript = Char.IsNumber(number) || Char.IsWhiteSpace(number) || number == '.' ? decript += number : decript += Convert.ToChar(letra);
+
+            }
+
+            return decript;
+        }
+
+        private void btnCifrar_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtDecifrado.Text))
+            {
+                MessageBox.Show("Informe o texto descifrado");
+            }
+            else
+            {
+               MessageBox.Show(Cifrar(txtDecifrado.Text, int.Parse(txtNumeroCasas.Text)));
+            }
+        }
+
+        private void btnHash_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtDecifrado.Text))
+            {
+                MessageBox.Show("Informe o texto descifrado");
+            }
+            else
+            {
+                txtResumo.Text = HashSHA1(txtDecifrado.Text);
+            }
+        }
+
+
+        public static string HashSHA1(string value)
+        {
+            var shaSHA1 = System.Security.Cryptography.SHA1.Create();
+            var inputEncodingBytes = Encoding.ASCII.GetBytes(value);
+            var hashString = shaSHA1.ComputeHash(inputEncodingBytes);
+
+            var stringBuilder = new StringBuilder();
+            for (var x = 0; x < hashString.Length; x++)
+            {
+                stringBuilder.Append(hashString[x].ToString("X2"));
+            }
+            return stringBuilder.ToString();
+
+        }
+
+        /// <summary>
+        /// depreciado (Usa form-data file)
+        /// </summary>
+        /// <param name="json"></param>
+        /// <param name="token"></param>
+        public void EnviarJson(Json json, string token)
+        {
+
+            string url = string.Format("https://api.codenation.dev/v1/challenge/dev-ps/submit-solution?token={0}", token);
+
+            WebRequest request = WebRequest.Create(url);
+
+            request.Method = "POST";
+
+
+            JavaScriptSerializer js = new JavaScriptSerializer();
+            string jsonData = js.Serialize(json);
+
+ 
+            byte[] byteArray = Encoding.UTF8.GetBytes(jsonData);
+
+
+            request.ContentType = "multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW";
+
+            request.ContentLength = byteArray.Length;
+
+ 
+            Stream dataStream = request.GetRequestStream();
+
+            dataStream.Write(byteArray, 0, byteArray.Length);
+
+            dataStream.Close();
+
+
+            WebResponse response = request.GetResponse();
+
+            var statusDescription = ((HttpWebResponse)response).StatusCode;
+
+
+            using (dataStream = response.GetResponseStream())
+            {
+
+                StreamReader reader = new StreamReader(dataStream);
+
+                var retorno = reader.ReadToEnd();
+ 
+            }
+
+        }
+
+        private void btnGerarJson_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                if (string.IsNullOrWhiteSpace(txtCifrado.Text) || string.IsNullOrWhiteSpace(txtDecifrado.Text) || string.IsNullOrWhiteSpace(txtResumo.Text))
+                {
+                    MessageBox.Show("Campos obrigatórios em branco");
+                }
+                else
+                {
+
+                    Json json = new Json();
+                    json.numero_casas = int.Parse(txtNumeroCasas.Text);
+                    json.token = txtToken.Text;
+                    json.cifrado = txtCifrado.Text;
+                    json.decifrado = txtDecifrado.Text;
+                    json.resumo_criptografico = txtResumo.Text;
+
+                    GerarJson(json);
+
+                    MessageBox.Show("Arquivo .json gerado com sucesso");
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Erro ao gerar arquivo .json");
+            }
+        }
+
+        private void GerarJson(Json json)
+        {
+
+            JavaScriptSerializer js = new JavaScriptSerializer();
+            string jsonData = js.Serialize(json);
+            System.IO.File.WriteAllText(@"C:\Users\izaias.costa\source\repos\Desafio\Desafio\bin\answer.json", jsonData);
+        }
+
+       
+    }
+}
